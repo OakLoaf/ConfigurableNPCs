6e180796989217dd0410d7803e536aa02b7857fa
OakLoaf <oak@beaconstudios.org>
2026-03-22 23:04:26 +0000

    Removed unnecessary fields

--- src/main/java/org/lushplugins/configurablenpcs/entity/meta/type/EntitySerializer.java
+++ src/main/java/org/lushplugins/configurablenpcs/entity/meta/type/EntitySerializer.java
@@ -10,11 +10,9 @@ import java.util.List;
 
 public class EntitySerializer implements EntityMetaSerializer<EntityMeta> {
     private static final BooleanField ON_FIRE = new BooleanField("on-fire");
-    private static final BooleanField SNEAKING = new BooleanField("sneaking");
     private static final BooleanField SPRINTING = new BooleanField("sprinting");
     private static final BooleanField INVISIBLE = new BooleanField("invisible");
     private static final BooleanField GLOWING = new BooleanField("glowing");
-    private static final BooleanField SWIMMING = new BooleanField("swimming");
     private static final BooleanField FLYING_WITH_ELYTRA = new BooleanField("flying-with-elytra");
     private static final ComponentField CUSTOM_NAME = new ComponentField("custom-name");
     private static final BooleanField CUSTOM_NAME_VISIBLE = new BooleanField("custom-name-visible");
@@ -29,11 +27,9 @@ public class EntitySerializer implements EntityMetaSerializer<EntityMeta> {
     @Override
     public void deserialize(ConfigurationSection config, EntityMeta meta) {
         meta.setOnFire(ON_FIRE.deserialize(config, false));
-        meta.setSneaking(SNEAKING.deserialize(config, false));
         meta.setSprinting(SPRINTING.deserialize(config, false));
         meta.setInvisible(INVISIBLE.deserialize(config, false));
         meta.setGlowing(GLOWING.deserialize(config, false));
-        meta.setSwimming(SWIMMING.deserialize(config, false));
         meta.setFlyingWithElytra(FLYING_WITH_ELYTRA.deserialize(config, false));
         meta.setCustomName(CUSTOM_NAME.deserialize(config, null));
         meta.setCustomNameVisible(CUSTOM_NAME_VISIBLE.deserialize(config, false));
@@ -44,11 +40,9 @@ public class EntitySerializer implements EntityMetaSerializer<EntityMeta> {
     @Override
     public void serialize(ConfigurationSection config, EntityMeta meta) {
         ON_FIRE.serialize(config, meta.isOnFire());
-        SNEAKING.serialize(config, meta.isSneaking());
         SPRINTING.serialize(config, meta.isSprinting());
         INVISIBLE.serialize(config, meta.isInvisible());
         GLOWING.serialize(config, meta.isGlowing());
-        SWIMMING.serialize(config, meta.isSwimming());
         FLYING_WITH_ELYTRA.serialize(config, meta.isFlyingWithElytra());
         CUSTOM_NAME.serialize(config, meta.getCustomName());
         CUSTOM_NAME_VISIBLE.serialize(config, meta.isCustomNameVisible());
@@ -59,7 +53,7 @@ public class EntitySerializer implements EntityMetaSerializer<EntityMeta> {
     @Override
     public List<Field<?>> getFields() {
         return List.of(
-            ON_FIRE, SNEAKING, SPRINTING, INVISIBLE, GLOWING, SWIMMING, FLYING_WITH_ELYTRA, CUSTOM_NAME,
+            ON_FIRE, SPRINTING, INVISIBLE, GLOWING, FLYING_WITH_ELYTRA, CUSTOM_NAME,
             CUSTOM_NAME_VISIBLE, SILENT, POSE
         );
     }
